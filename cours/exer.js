57f19617e2b7168c239cc2f007f06a0276ce8999
nico12354 <mine-nico@hotmail.com>
2019-05-31 20:00:26 +0200

    external

--- cours/exer.js
+++ cours/exer.js
@@ -1,9 +1,12 @@
 document.write("question is :")
 
 
+var exercice = require(./histoire.js)
+document.write("hello")
+document.write(exercice[0][0])
+
 function clic() {
   document.write("hello")
 }
 
 
-object.onclick = clic();
