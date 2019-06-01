2c5cb8051c3952f9265e597d57a487af3b506938
nico12354 <mine-nico@hotmail.com>
2019-06-01 17:10:00 +0200

    t

--- cours/exer.js
+++ cours/exer.js
@@ -1,9 +1,14 @@
-document.write("test 5");
 var histoire = [
-["what is that ?","a chicken" ],
-["who is that ?","me"]
+["Citer les différentes raisons qui expliquent l'émergence de l'extrèment droite en Europe durant la période des années 30","- Le Krach boursier de 1929 <br> - Les pays touchés douvent faire face à une augmentation du chômage, une diminution de la production et de la valeur des titres boursiers. Comme ces pays sont libéralistes, l'Etat ne peut intervenir <br> - Mise en place du protectionnisme et de la dévaluation de la monnaie, ce qui aggrave la situation <br> - Défaite et diktat de Versailles = arrivée de régime Totalitaires <br> - Peur du communisme"],
+["Citer l'exemple de huit pays et leader fascistes des années 30","- Léon Degrelle (Belgique) <br> - Benito Mussolini (Italie) <br> - Adolf Hitler (Allemagne) <br> - Géneral Franco (Espagne) <br> - Oswald Mosley (Angleterre) <br> - Charles Maurras (France) <br> - Marechal Pilsudski (Pologne) <br> - Amiral Horthy (Hongrie)"],
+["Citer l'ensemble des éléments qui expliquent l'émergence du fascisme italien","- Crise générale (militaire, politique et sociale) après défaite guerre <br> - Honte pour les Italiens dû aux défaites de la 1ère guerre mondiale (Caporetto 1917) <br> - Guerre civile 1922 (défaite des socialistes) car chemises noires combattent violemment pr défendre nationalisme italien (la gauche) <br> Partit fasciste prend le pouvoir par la force (marche sur rome 1922 25000 fasciste exigent le pouvoir) <br> - Remporte élection 1924 car guerre civile (répression)"],
+[" "," "],
+[" "," "],
+[" "," "]
+
+
+//[" "," "],
 ]
-//document.write(histoire.length)
 
 var question = document.getElementById("q");
 var response = document.getElementById("r");
@@ -15,7 +20,7 @@ var lq = (histoire.length - 1);
 
 counter.innerHTML = (nq+1) + " / " + (lq+1)
 question.innerHTML = histoire[nq][0];
-response.innerHTML = " ";
+response.innerHTML = " / ";
 
 function nextquestion () {
 
@@ -24,10 +29,10 @@ nq = -1
 }
 
 nq += 1;
-counter.innerHTML = (nq+1) + "/" + (lq+1)
+counter.innerHTML = (nq+1) + " / " + (lq+1)
 
 question.innerHTML = histoire[nq][0];
-response.innerHTML = " ";
+response.innerHTML = " / ";
 }
 
 function reveal () {
